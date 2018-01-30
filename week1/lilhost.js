f9d46d20062b9bfa611e8c1472111c173a074c16
calebfergie <yonohabloespanol@gmail.com>
2018-01-30 01:37:03 -0500

    second commit

--- week1/lilhost.js
+++ week1/lilhost.js
@@ -9,34 +9,36 @@ var url = require('url');
 
 function requestHandler(req, res) {
 
-	var parsedUrl = url.parse(req.url);
-	console.log("The Request is: " + parsedUrl.pathname);
-		
-	fs.readFile(__dirname + parsedUrl.pathname, 
-		// Callback function for reading
-		function (err, data) {
-			// if there is an error
-			if (err) {
-				res.writeHead(500);
-				return res.end('Error loading ' + parsedUrl.pathname);
-			}
-			// Otherwise, send the data, the contents of the file
-			res.writeHead(200);
-			res.end(data);
-  		}
-  	);
+  var parsedUrl = url.parse(req.url);
+  console.log("The Request is: " + parsedUrl.pathname);
+
+  fs.readFile(__dirname + parsedUrl.pathname,
+    // Callback function for reading
+    function(err, data) {
+      // if there is an error
+      if (err) {
+        res.writeHead(500);
+        return res.end('Error loading ' + parsedUrl.pathname);
+      }
+      // Otherwise, send the data, the contents of the file
+      res.writeHead(200);
+      res.end(data);
+    }
+  );
 }
 
 /* var submissionButton
 submitButton.addEventListener('click', logSubmission);
 {
    submissionButton = document.getElementById('submitButton');
-}*/
+}
 
 function init() {
 	submissionButton = document.getElementById('submitButton');
     alert(submissionButton.innerHTML);
 }
-window.addEventListener('load', init);	
+window.addEventListener('load', init);
+
+*/
 
 lilserv.listen(8080);
